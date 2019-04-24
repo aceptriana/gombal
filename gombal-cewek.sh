da27be3ab012407a4d930b5587436e78cef9e13a
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-04-25 04:43:36 +0700

    Add files via upload

---
+++ gombal-cewek.sh
@@ -0,0 +1,65 @@
+GNU nano 4.1                                   lope.sh
+#!/usr/bin/bash
+#tools sederhana
+#bucin
+#by Holilul Anwar
+#by Acep X-Code
+#Direkode Rusak
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+
+
+
+echo " "
+echo $red "oo"
+echo $red "   oo     OOOOOOOO:       OOOOOOOO!"
+echo $red "      oOOOO!!!!::::O    OO.......::!O"
+echo $red "     OOO!!!::::::::O  O.......:   : !O"
+echo $red "     OOO!!!!::::::::.OO........:    :!O"
+echo $red "     OO!!!!:::::::..............:   :!O"
+echo $red "     OOO!!!:::::::..............:   :!O"
+echo $red "      OO!!::::::::.............:   :!O"
+echo $red "       OO!::::::::......oo.....::::!O"
+echo $red "         O!!:::::::........oo..:::O"
+echo $red "           !!!::::::..........ooO"
+echo $red "              !!::::::.......O   oo"
+echo $red "                ::::::.....O        oo  .o"
+echo $red "                   :::..O              ooo"
+echo $red "                     ::.              oooo"
+echo $red "                      :"
+echo " "
+
+echo  $red "Hay Cantik"
+sleep 1
+echo  $cyan "Siapa Nama kamu?"
+read -p "Masukkan Nama==>" na;
+sleep 3
+echo " "
+echo " "
+echo  $blue "Hay $na"
+
+
+
+echo " "
+echo $red "oo"
+echo $red "   oo     OOOOOOOO:       OOOOOOOO!"
+echo $red "      oOOOO!!!!::::O    OO.......::!O"
+echo $red "     OOO!!!::::::::O  O.......:   : !O"
+echo $red "     OOO!!!!::::::::.OO........:    :!O"
+echo $red "     OO!!!!:::::::..............:   :!O"
+echo $red "     OOO!!!:::::::..............:   :!O"
+echo $red "      OO!!::::::::.............:   :!O"
+echo $red "       OO!::::::::......oo.....::::!O"
+echo $red "         O!!:::::::........oo..:::O"
+echo $red "           !!!::::::..........ooO"
+echo $red "              !!::::::.......O   oo"
+echo $red "                ::::::.....O        oo  .o"
+echo $red "                   :::..O              ooo"
+echo $red "                     ::.              oooo"
+echo $red "                      :"
+sleep 1
